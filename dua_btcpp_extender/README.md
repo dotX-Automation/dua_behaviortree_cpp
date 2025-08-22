64f9c4b92b1cdf0112d1621386e1eb7e8e83c84d
Roberto Masocco <robmasocco@gmail.com>
2025-08-22 18:33:01 +0200

    feat: dua_btcpp_extender

---
+++ dua_btcpp_extender/README.md
@@ -0,0 +1,19 @@
+# dua_btcpp_extender
+
+Base package for extending the `dua_btcpp` library, adding more custom BT.CPP nodes to the `btcpp_executor`.
+
+Based on [`pluginlib`](https://docs.ros.org/en/jazzy/Tutorials/Beginner-Client-Libraries/Pluginlib.html).
+
+---
+
+## Copyright and License
+
+Copyright 2025 dotX Automation s.r.l.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and limitations under the License.
---
+++ dua_btcpp_extender/include/dua_btcpp_extender/extender_base.hpp
@@ -0,0 +1,74 @@
+/**
+ * Extender base class to add more custom BT.CPP nodes to the btcpp_executor.
+ *
+ * dotX Automation s.r.l. <info@dotxautomation.com>
+ *
+ * August 22, 2025
+ */
+
+/**
+ * Copyright 2025 dotX Automation s.r.l.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <dua_btcpp/entity_manager.hpp>
+
+#include <behaviortree_cpp/behavior_tree.h>
+#include <behaviortree_cpp/bt_factory.h>
+
+namespace dua_btcpp_extender
+{
+
+/**
+ * Allows to add more custom BT.CPP nodes.
+ */
+class ExtenderBase
+{
+public:
+  /**
+   * @brief Destructor.
+   */
+  virtual ~ExtenderBase() {}
+
+  /**
+   * @brief Registers custom nodes with the BehaviorTreeFactory (to be overridden).
+   *
+   * @param factory Pointer to the BT.CPP factory to use.
+   * @param node Pointer to the ROS 2 node to use.
+   * @param entity_manager Pointer to the entity manager to use.
+   * @param wait_servers Enables waiting for service/action servers upon client creation.
+   * @param spin Enables spinning the ROS 2 node when client nodes call services/actions.
+   * @throws std::runtime_error if any node fails to register.
+   * @throws std::bad_any_cast if clients instantiation is misconfigured.
+   */
+  virtual void register_nodes(
+    BT::BehaviorTreeFactory & factory,
+    std::shared_ptr<dua_node::NodeBase> node,
+    std::shared_ptr<dua_btcpp::EntityManager> entity_manager,
+    bool wait_servers,
+    bool spin) = 0;
+
+protected:
+  /**
+   * @brief Constructor, required by pluginlib to be protected and noop.
+   */
+  ExtenderBase()
+  {}
+};
+
+} // namespace dua_btcpp_extender
